6967d7826b9a6b0c6574c7fd5b4f1296ede9dd75
Yooni <yooni.kj.yoon@gmail.com>
2023-02-28 16:28:54 +0900

    JS Home transparent

--- main.js
+++ main.js
@@ -28,6 +28,14 @@ contactButton.addEventListener("click", () => {
   scrollIntoView("#contact");
 });
 
+// Make home slowly fade to transparent as the window scrolls down.
+const home = document.querySelector(".home__container");
+const homeHeight = home.getBoundingClientRect().height;
+document.addEventListener("scroll", () => {
+  // 공식 사용 : (1 - window.scrollY / homeHeight) => 투명도에 넣기
+  home.style.opacity = 1 - window.scrollY / homeHeight;
+});
+
 function scrollIntoView(selector) {
   const scrollTo = document.querySelector(selector);
   scrollTo.scrollIntoView({ behavior: "smooth" });
